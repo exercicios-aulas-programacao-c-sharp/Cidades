fd50d5b8a11f6c210f838cc31dcd39821a08d091
Prof. Diego Neri <diego@diegoneri.com>
2021-02-26 23:14:08 -0300

    Finaliza o programa Cidades

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Cidades
+{
+    class Program
+    {
+        //Faça um programa que exiba os nomes das cidades do litoral sul de São Paulo, um em cada linha.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\\-------------------------\\");
+            Console.WriteLine("|  Cidades do Litoral Sul |");
+            Console.WriteLine("\\-------------------------\\\n");
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            //Console.WriteLine("Peruíbe, \"no rio dos tubarões\"");
+
+            Console.Write("Peruíbe,\t");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("\"no rio dos tubarões\"");
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Itanhaém,\t\"pedra que canta\"");
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Mongaguá,\t\'água pegajosa\'");
+            Console.ResetColor(); 
+        }
+    }
+}
